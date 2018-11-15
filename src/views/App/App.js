d037691bf378d307889288eac7e91e5fa1744856
Vladislav Kurochkin <vlad555.95@mail.ru>
2018-11-16 00:36:16 +0400

    Initial

---
+++ src/views/App/App.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import './App.styles';
+import {Wrapper} from "./App.styles";
+
+class App extends Component {
+  render() {
+    return (
+      <Wrapper>Hello!</Wrapper>
+    );
+  }
+}
+
+export default App;
